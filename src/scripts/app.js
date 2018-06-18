e8e1d69887ca171c1aa5f4bf4772e158dba52c56
xmd5a <zakaru@gmail.com>
2018-06-18 22:17:23 +0200

    make inputs more generic, toggle/collapse js, new inputs types

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,3 +2,9 @@
   Project: frontend-ui-dev-recruitment-task
   Author: Piotr Szarmach
  */
+
+import {togglePanel} from './modules/toggle';
+
+document.querySelectorAll(".c-section__headline").forEach((el) => {
+  el.addEventListener("click", togglePanel);
+});
---
+++ src/scripts/modules/toggle.js
@@ -0,0 +1,4 @@
+export const togglePanel = (el) => {
+  const section = el.currentTarget.getAttribute("data-section");
+  document.querySelector(`div[data-section="${section}"]`).classList.toggle("active");
+};
